566c79a64d68ea5428058b3459cfde115ecae0d4
chandrikathadi <chandrikathadi2005@gmail.com>
2025-03-09 20:29:50 +0530

    Add files via upload

---
+++ swaping.py
@@ -0,0 +1,33 @@
+
+a=5
+b=6
+print("value before swap a=",a,"\t","b=",b)
+c=a
+a=b
+b=c
+
+print("value after swap a=",a,"\t","b=",b)
+print(".............")
+a=10
+b=20
+print("value before swap a=",a,"\t","b=",b)
+a=a+b
+b=a-b
+a=a-b
+print("value after swap a=",a,"\t","b=",b)
+print(".............")
+a=15
+b=45
+print("value before swap a=",a,"\t","b=",b)
+a=a*b
+b=a//b
+a=a//b
+print("value after swap a=",a,"\t","b=",b)
+print(".............")
+a=33
+b=13
+print("value before swap a=",a,"\t","b=",b)
+a=a^b
+b=a^b
+a=a^b
+print("value after swap a=",a,"\t","b=",b)
