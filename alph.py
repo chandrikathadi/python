1351dd16f3aaa5f32ce729daf95942eba159fe0c
chandrikathadi <chandrikathadi2005@gmail.com>
2025-03-13 22:40:29 +0530

    Add files via upload

---
+++ alph.py
@@ -0,0 +1,9 @@
+ch=input("press any key")
+if(ch>="A" and ch<="Z"):
+  print("upper case")
+elif(ch>="a" and ch<="z"):
+  print("lower case")
+elif(ch>="0" and ch<="9"):
+  print("digit")
+else:
+  print("special character")
