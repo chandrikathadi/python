c8f1028614876d89dda2a641ea585ab076dec01b
chandrikathadi <chandrikathadi2005@gmail.com>
2026-05-23 10:28:10 +0530

    Add files via upload

---
+++ run_time1.py
@@ -0,0 +1,29 @@
+"""a=int(input("enter a: "))
+b=int(input("enter b: "))
+print(a+b)
+
+str
+a=input("data1  :  ")
+b=input("data2  :  ")
+print(a+b)
+
+
+float
+a=float(input("enter a : "))
+b=float(input("enter b : "))
+print(a+b)
+
+bool
+a=bool(input("enter a:  "))
+b=bool(input("enter b:  "))
+print(a+b)
+
+
+complex
+a=complex(input("enter a: "))
+b=complex(input("enter b: "))
+print(a+b)"""
+
+a=int(input("enter the value a"))
+b=int(input("enter the value b"))
+print(a+b)
